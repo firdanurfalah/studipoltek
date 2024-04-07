bd3a589d4a6129eb0bfc6080e9367a1ae0dc0da6
dannykoes <sandal2610@gmail.com>
2024-04-07 22:38:03 +0700

    artikle image

--- resources/views/admin/pages/artikel/index.blade.php
+++ resources/views/admin/pages/artikel/index.blade.php
@@ -8,9 +8,9 @@
 
         <div class="page-header">
             <div class="page-title">
-              
+
                 <a href="/artikel/create" class="btn btn-primary btn-sm">Tambah</a>
-              
+
             </div>
         </div>
 
@@ -24,14 +24,14 @@
                         <table id="zero-config" class="table dt-table-hover" style="width:100%">
                             <thead>
                                 <tr>
-                                 
-                                   
+
+
                                     <th>gambar</th>
                                     <th>judul</th>
                                     <th>deskripsi</th>
-                                   
-                                   
-                                   
+
+
+
 
                                     <th class="no-content text-center">Aksi</th>
                                 </tr>
@@ -39,24 +39,27 @@
                             <tbody>
                                 @foreach($artikel as $key => $value)
                                 <tr>
-                                 
-                                
-                                    <td> <img  src="/gambar?rf={{$value->gambar}}"> </td>
+
+
+                                    <td> <img width="120px" src="/gambar?rf={{$value->gambar}}"
+                                            onclick="showImage('{{$value->gambar}}')"> </td>
                                     <td>{{$value->judul}}</td>
                                     <td>{{$value->deskripsi}}</td>
-                                   
-                                  
-                          
-                          
+
+
+
+
 
 
                                     <td>
-                                        <a href="/artikel/{{$value->id}}" class="btn btn-primary btn-sm d-block d-none">Edit</a>
-                                        <br> 
-                                       <form action="{{ route('artikel.destroy', $value->id) }}" method="post">
+                                        <a href=" /artikel/{{$value->id}}"
+                                            class="btn btn-primary btn-sm d-block d-none">Edit</a>
+                                        <br>
+                                        <form action="{{ route('artikel.destroy', $value->id) }}" method="post">
                                             @method('DELETE')
                                             @csrf
-                                            <a type="submit" class="btn btn-danger btn-sm show_confirm d-block d-none">Hapus</a>
+                                            <a type="submit"
+                                                class="btn btn-danger btn-sm show_confirm d-block d-none">Hapus</a>
                                         </form>
                                     </td>
                                 </tr>
@@ -71,7 +74,19 @@
 
         </div>
 
-
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Detail Image</h5>
+                    </div>
+                    <div class="modal-body text-center">
+                        <img src="" alt="" id="imagecategori" width="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
         <!-- CONTENT AREA -->
@@ -79,4 +94,10 @@
     </div>
 </div>
 <!--  END CONTENT AREA  -->
+<script>
+    function showImage(foto) {
+        $('#imagecategori').attr('src','/gambar?rf=/'+foto);
+        $('#exampleModal').modal('show');
+    }
+</script>
 @endsection
